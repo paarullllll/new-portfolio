1bd77b28cf6041fa9bdc2643cc2a4dd502d5225c
Parul Chouhan <92985366+paarullllll@users.noreply.github.com>
2024-01-13 21:00:46 +0530

    Add files via upload

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from "react";
+import Loader from "../Loader";
+
+interface ButtonProps {
+	children: string | ReactNode;
+	classes?: string;
+	onClick?: React.MouseEventHandler<HTMLElement> | undefined;
+	type?: "button" | "submit" | "reset" | undefined;
+	disabled?: boolean;
+	isLoading?: boolean;
+}
+
+const Button = (props: ButtonProps) => {
+	const { children, classes, onClick, type, disabled, isLoading } = props;
+	return (
+		<button
+			onClick={onClick}
+			style={{ borderRadius: "10px" }}
+			className={`bg-primary hover:bg-primary-hover text-secondary text-base font-semibold px-10 py-3  flex items-center justify-center ${
+				disabled || isLoading ? "cursor-not-allowed opacity-50" : ""
+			} ${classes}`}
+			type={type}
+			disabled={disabled}
+		>
+			{isLoading ? <Loader bg="#000" color="#000" /> : null}
+			{children}
+		</button>
+	);
+};
+
+export default Button;
